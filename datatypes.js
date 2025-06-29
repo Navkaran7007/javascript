7071d442136e2f3f4bb615f02d930aa722310b64
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-06-29 16:47:09 -0500

    Initial COmmit

---
+++ datatypes.js
@@ -0,0 +1,42 @@
+/**
+ * Number = 4
+ * String = 'Nav'
+ * Booleans = true/fales
+ * null = standalone value (Its type is object )
+ * undefined = 
+ * symbol = unique
+ * 
+ * All types except Object define immutable values represented directly at the lowest level of the language. 
+ * We refer to values of these types as primitive values.
+
+All primitive types, except null, can be tested by the typeof operator. typeof null returns "object", 
+so one has to use === null to test for null.
+*/
+
+let a = '3';
+console.log( );
+
+
+let x = 5;
+x = 'hello';
+
+
+
+
+/**
+ * Most Important 
+ * Optional Chaining
+ * let user = {
+  name: "Alice",
+  address: {
+    city: "Paris"
+  }
+};
+
+console.log(user.address?.city); // Paris ✅
+console.log(user.profile?.bio);  // undefined ✅ (no error)
+
+This basically mean if we access for example an array withoutoptional Chaining and if we are accessing an array property that does not exits
+ this will give an error
+but with optional chaining (?.) we get undefined as outcome.
+ */
---
+++ variables.js
@@ -0,0 +1,12 @@
+/**
+ * Variables are like container which are used to store data
+ */
+const accountID = 7007;
+let accountName = 'Nav';
+var accountMail = 'Gmail';
+accountPlace = 'Moga';
+
+accountName= 'simu';
+accountPlace = 'Winnipeg';
+let accountState;
+console.table([accountID, accountMail,accountName,accountPlace,accountState])
