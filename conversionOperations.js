ab5540ad0141b5d1f76536b47276df19e9dbd112
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-06-29 17:29:35 -0500

    conversion of datatypes

---
+++ conversionOperations.js
@@ -0,0 +1,19 @@
+let score = 33;
+
+// console.log(typeof(score));
+
+// // Converting To string
+// let valueInstring = Number(score);
+// console.log(typeof valueInstring);
+// console.log(valueInstring)
+
+/**
+ * "33" =. 33
+ * "33abc" => Nan
+ * true = 1
+ */
+
+let isLoggerIn = '';
+
+let typeCoversion = Boolean(isLoggerIn)
+console.log(typeCoversion)
